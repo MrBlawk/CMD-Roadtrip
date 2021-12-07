bbef789fc1cbbe1bee8d295e8f86534450e5966e
Blawk <stijn.timmerman@student.nhlstenden.com>
2021-12-07 14:40:41 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ scripts/level_two.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("textContainer");
+
+let jobsArray = [
+    "Channelmanager",
+    "Consultant nieuwe media",
+    "Game Designer",
+    "Multimediaontwerper",
+    "Project Manager",
+    "Webdesigner",
+    "Fotograaf",
+];
+
+for(let i = 0; i < 4; i++){
+    for(let i = 0; i < jobsArray.length; i++){
+        container.innerHTML += "<p>" + jobsArray[i] + "</p>"
+    
+        if(i === jobsArray.length){
+            i = 0;
+        }
+    }
+}
+
+
+
+
