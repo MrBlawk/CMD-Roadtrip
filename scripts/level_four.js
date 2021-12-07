e8142a92a5aeaed98bfa9ee29be4fa653980e0b4
Bram <bram.schrijver@student.nhlstenden.com>
2021-12-07 23:03:26 +0100

    Three.js imported

--- scripts/level_four.js
+++ scripts/level_four.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.js";
+import * as THREE from '../node_modules/three/build/three.module.js';
 
 
 var scene, camera, hlight;
@@ -25,7 +25,7 @@ function init() {
     hlight = new THREE.AmbientLight(0xffffff, 1);
     scene.add(hlight);
 
-    renderer = new THREE.WebGLRenderer({canvas});
+    var renderer = new THREE.WebGLRenderer({canvas});
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
@@ -39,7 +39,44 @@ function init() {
     camera.updateProjectionMatrix();
     }
 
+    // resize
+    window.addEventListener( 'resize', function(){
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        renderer.setSize( width, height );
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix( );
+    } );
+
+    // geometry added
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+
+    // material, color
+    var material = new THREE.MeshBasicMaterial( {color: 0xFFFFFF, wireframe: true } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add(cube);
+
+    camera.position.z = 3;
+
+    var update = function( ){
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.005; 
+    };
+
+    var render = function( ){
     renderer.render(scene, camera);
+    };
+
+    var GameLoop = function ( ){
+        requestAnimationFrame( GameLoop );
+
+        update( );
+        render( );
+    };
+
+    GameLoop( );
+
+  
 
     requestAnimationFrame(render);
     }
