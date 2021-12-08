d688b3e8031c992a92e4ac8d2326c18f11f1dfb4
Stijn <stijn.timmerman@student.nhlstenden.com>
2021-12-08 13:49:46 +0100

    car animation

--- scripts/level_three.js
+++ scripts/level_three.js
@@ -1,28 +1,8 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger"
-
-gsap.registerPlugin(ScrollTrigger);
-
 
 let sections = gsap.utils.toArray(".city");
-
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#levelThree",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: "+=3500",
-  }
-});
-
-
-let sections = gsap.utils.toArray(".city");
-console.log(sections.length + " sections detected.");
-
+let car = document.querySelector(".van-img");
+let scrollLength = 3500;
+car.autoAlpha = 0;
 
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
@@ -32,9 +12,37 @@ gsap.to(sections, {
     start: "0",
     pin: true,
     scrub: 1,
-    snap: 1 / (sections.length - 2),
+    snap: 1 / (sections.length -1),
     // base vertical scrolling on how wide the container is so it feels more natural.
-    end: "+=3500",
+    end: "+=" + scrollLength,
+    markers: true
   }
 });
+
+
+gsap.to(car, {
+    xPercent: window.innerWidth - 100,
+    scrollTrigger:{
+        trigger: "#levelThree",
+        ease: "power2.out",
+        start: "0",
+        scrub: 2,
+        end: "+=" + scrollLength,
+        markers: true
+    }
+})
+
+gsap.to(car, {
+    autoAlpha: 0,
+    scrollTrigger:{
+        trigger: "#levelFour",
+        start: "-=1000",
+        scrub: 1,
+        markers: true,
+        end: "+=50",
+
+    }
+})
+
+
 
