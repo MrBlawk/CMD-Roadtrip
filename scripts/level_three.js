0a06d259e923a2c46e17dbb4865cf82d4705459f
Bram <bram.schrijver@student.nhlstenden.com>
2021-12-08 13:18:08 +0100

    test push

--- scripts/level_three.js
+++ scripts/level_three.js
@@ -1,6 +1,28 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+let sections = gsap.utils.toArray(".city");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#levelThree",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=3500",
+  }
+});
+
 
 let sections = gsap.utils.toArray(".city");
 console.log(sections.length + " sections detected.");
+
 
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
