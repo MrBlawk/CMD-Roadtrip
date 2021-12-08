7cb5613bf49603648692885ad797b3274e1688ee
Stijn <stijn.timmerman@student.nhlstenden.com>
2021-12-08 13:05:08 +0100

    start on parallax

--- scripts/level_three.js
+++ scripts/level_three.js
@@ -1,11 +1,13 @@
 
 let sections = gsap.utils.toArray(".city");
+console.log(sections.length + " sections detected.");
 
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
   ease: "none",
   scrollTrigger: {
-    trigger: "#container",
+    trigger: "#levelThree",
+    start: "0",
     pin: true,
     scrub: 1,
     snap: 1 / (sections.length - 1),
