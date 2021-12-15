92f6f20491d5d9217847495dc24aac2839f3bae7
Stijn <stijn.timmerman@student.nhlstenden.com>
2021-12-15 16:25:11 +0100

    cloud generation done

--- scripts/level_three.js
+++ scripts/level_three.js
@@ -1,10 +1,11 @@
 
 let sections = gsap.utils.toArray(".city");
 let car = document.querySelector(".van-img");
+let obj;
 let scrollLength = 3500;
 car.autoAlpha = 0;
 
-let bg = document.getElementById("bg-parallax");
+let bg = document.getElementById("bgLayer");
 
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1.5),
@@ -54,6 +55,62 @@ gsap.to(bg, {
         end: "+=" + scrollLength,
     }
 })
+
+//cloud generating code
+
+let cloud;
+let container;
+function CloudEngine(){
+    obj = "<img id='cloud' src='./img/cloud.png' alt=''>"
+    container = document.getElementById("cloudLayer");  
+    container.innerHTML += obj;  
+    cloud = document.getElementById("cloud");
+    cloud.style.left = "110%";
+    translate(cloud, "-500");
+
+}
+
+function translate(e, x){
+    console.log("x is: " + x);
+    var left = parseInt( css( e, 'left' ), 10 ),
+        dx = left - x,
+        i = 1,
+        count = 700,
+        delay = 20;
+
+    function loop() {
+        
+        if(e.style.left === -500 )
+        {
+            console.log("x point reached");
+            e = null;
+            CloudEngine();
+        }
+
+        if ( i >= count ) { return; }
+        if(i === count -1){
+            container.removeChild(cloud);
+            CloudEngine();
+        }
+        i += 1;
+        e.style.left = ( left - ( dx * i / count ) ).toFixed( 0 ) + 'px';
+        setTimeout( loop, delay );
+        console.log(e.style.left)
+  
+
+    }
+    
+    loop();
+
+
+}
+
+function css( element, property ) {
+    return window.getComputedStyle( element, null ).getPropertyValue( property );
+}
+
+CloudEngine();
+
 
 
 
