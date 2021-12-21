e3a1f7b9dd5a0ce24754c95f2f07211a18dfc158
Blawk <stijnstijn11@gmail.com>
2021-12-21 14:13:38 +0100

    starry sky added

--- scripts/level_four.js
+++ scripts/level_four.js
@@ -1,48 +1,68 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 //import { GLTFLoader } from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
+import StarrySkyShader from './StarrySkyShader.js';
 
-
-
-
+//create array to fill with planetobjects later, minor array keeps track of the amount of minors that have to be listed
 var planets = [];
 var minors = ["artandsound", "concepting", "gd3d", "bad", "ondernemen", "sds", "tnw", "neuromarketing"];
 
-var raycaster = new THREE.Raycaster();
+//track mouse as a vector2 position (x,y)
 var mouse = new THREE.Vector2();
 
 
 var scene = new THREE.Scene( ); 
-    var camera =  new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 500);
+    var camera =  new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 500);
     // camera position
-    camera.position.set(0, 15, 50)
+    camera.position.set(0, 15, 30)
 
 
 
-    //skybox stuff
-    //settingup the materialarray and defining the textures to a let.
-    let materialArray = [];
-    let texture_ft = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
-    let texture_rt = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
-    let texture_lt = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
-    let texture_up = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
-    let texture_dwn = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
-    let texture_bk = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
+    // //skybox stuff
+    // //settingup the materialarray and defining the textures to a let.
+    // let materialArray = [];
+    // let texture_ft = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
+    // let texture_rt = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
+    // let texture_lt = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
+    // let texture_up = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
+    // let texture_dwn = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
+    // let texture_bk = new THREE.TextureLoader().load('./img/cubemap/sterren-bg.jpeg');
 
-    //pushing the materials to the array.
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}));
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}));
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_lt}));
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}));
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_dwn}));
-    materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}));
+    // //pushing the materials to the array.
+    // materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}));
+    // materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}));
+    // materialArray.push(new THREE.MeshBasicMaterial({map: texture_lt}));
+    // materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}));
+    // materialArray.push(new THREE.MeshBasicMaterial({map: texture_dwn}));
+    // materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}));
 
-    for(let i=0;i<6;i++)
-        materialArray[i].side = THREE.BackSide;
+    // for(let i=0;i<6;i++)
+    //     materialArray[i].side = THREE.BackSide;
 
-    let skyboxGeo = new THREE.BoxGeometry(100,100,100);
-    let skybox = new THREE.Mesh(skyboxGeo,materialArray);
-    scene.add(skybox);
+    // let skyboxGeo = new THREE.BoxGeometry(100,100,100);
+    // let skybox = new THREE.Mesh(skyboxGeo,materialArray);
+    // scene.add(skybox);
+
+    //skybox with star shader
+    var skyDomeRadius = 300;
+    var sphereMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+        skyRadius: { value: skyDomeRadius },
+        env_c1: { value: new THREE.Color('#0d1a2f') },
+        env_c2: { value: new THREE.Color('#ea8ff2') },
+        noiseOffset: { value: new THREE.Vector3(10, 100.01, 100.01) },
+        starSize: { value: 0.01 },
+        starDensity: { value: 0.09 },
+        clusterStrength: { value: 0.2 },
+        clusterSize: { value: 0.1 },
+    },
+    vertexShader: StarrySkyShader.vertexShader,
+    fragmentShader: StarrySkyShader.fragmentShader,
+    side: THREE.DoubleSide,
+    });
+    var sphereGeometry = new THREE.SphereGeometry(skyDomeRadius, 40, 40);
+    var skyDome = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    scene.add(skyDome);
 
     var renderer = new THREE.WebGLRenderer( {alpha:true, antialias:true} );
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -83,8 +103,8 @@ var scene = new THREE.Scene( );
 
     //custom positions for each planet
     var planetX = [-18,     -10,      -6,     -3,     5,    8,   15,    20]
-    var planetY = [0,       10,      -1,      8,     16,    0,   15,    2]
-    var planetZ = [0,       10,      -10,     -5,    -8,     0,   5,    0]
+    var planetY = [-10,       10,      -5,      3,     12,    -3,   13,    2]
+    var planetZ = [0,       10,      -10,     -5,    -8,     0,   5,    3]
 
     function createSpheres(type){
         for(let i = 0; i < minors.length; i++)
@@ -121,12 +141,13 @@ var scene = new THREE.Scene( );
 
         function createMesh(texture, x, y, z){
             var geometry = new THREE.SphereGeometry(2, 32, 16);
-            var material = new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('./img/Minor_Planet/Planeet_Textures/' + texture + ".png")})
+            var material = new THREE.MeshLambertMaterial({map: new THREE.TextureLoader().load('./img/Minor_Planet/Planeet_Textures/' + texture + ".png")})
             var sphere = new THREE.Mesh(geometry, material);
             sphere.rotation.x = Math.random() * (20 - 0);
             sphere.position.set(x, y, z)
             sphere.userData.name = texture;
-            console.log(sphere.userData.name)
+            sphere.castShadow = true;
+            sphere.receiveShadow = true;
             planets.push(sphere);
             scene.add(sphere);
         }
@@ -136,19 +157,19 @@ var scene = new THREE.Scene( );
 
     //creating lightsources for the scene
     var sLight = new THREE.SpotLight(0xFFFFFF, 1);
-    sLight.position.set(-99,100,99)
-
+    sLight.position.set(-40,80,30)
     sLight.castShadow = true;
 
     sLight.shadow.mapSize.width = 1024;
     sLight.shadow.mapSize.height = 1024;
 
-    sLight.shadow.camera.near = 500;
-    sLight.shadow.camera.far = 4000;
+    sLight.shadow.camera.near = 1;
+    sLight.shadow.camera.far = 2000;
     scene.add(sLight);
 
-    var ambientLight = new THREE.AmbientLight( 0xFFFFFF, 0.5)
-    scene.add( ambientLight );
+    var aLight = new THREE.AmbientLight( 0xFFFFFF, 0.3)
+    aLight.castShadow = true;
+    scene.add( aLight );
 
 
 
