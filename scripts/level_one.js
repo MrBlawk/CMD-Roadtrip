295b271a9615ed14e598ef3d7268cff70e42fe52
Blawk <stijnstijn11@gmail.com>
2021-12-23 10:45:00 +0100

    styling deels gefixt

--- scripts/level_one.js
+++
@@ -1,9 +0,0 @@
-function enlistFunction(){
-    document.getElementById("levelTwo").scrollIntoView({behavior: 'smooth'});
-}
-
-function startExperience(){
-    document.getElementById("levelThree").scrollIntoView({behavior: 'smooth'});
-}
-
-
